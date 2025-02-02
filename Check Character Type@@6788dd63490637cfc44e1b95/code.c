3854466c0727d2d6f830aa7a8b459cb6dd3ada5d
ANSHIKA1058 <anshika1058.becse24@chitkara.edu.in>
2025-02-02 20:20:13 +0530

    Solution to Check Character Type

--- Check Character Type@@6788dd63490637cfc44e1b95/code.c
+++ Check Character Type@@6788dd63490637cfc44e1b95/code.c
@@ -10,6 +10,9 @@ int main() {
    else if(ch=='b' || ch=='c' || ch=='d' || ch=='f' || ch=='g' || ch=='h' || ch=='j' || ch=='k' || ch=='l' || ch=='m' || ch=='n' || ch=='p' || ch=='q' || ch=='r' || ch=='s' || ch=='t' ||ch=='v' || ch=='w' || ch=='x'|| ch=='y' || ch=='z'){
     printf("Consonant");
    }
+   else if(ch>=0){
+    printf("Digit");
+   }
    else{
     printf("Special Character");
    }
