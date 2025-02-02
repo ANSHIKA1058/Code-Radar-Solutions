750c60eae0d6831cdf41ef4289804f856f4dbb1d
ANSHIKA1058 <anshika1058.becse24@chitkara.edu.in>
2025-02-02 20:17:42 +0530

    Solution to Check Character Type

--- Check Character Type@@6788dd63490637cfc44e1b95/code.c
+++ Check Character Type@@6788dd63490637cfc44e1b95/code.c
@@ -7,10 +7,10 @@ int main() {
    if(ch=='a' || ch=='e' || ch=='i' || ch=='o' || ch=='o'){
     printf("Vowel");
    }
-   else if(ch=='*' || ch=='/' || ch=='-' || ch=='+' || ch=='!' || ch=='@' || ch=='#' || ch=='$' || ch=='%' || ch=='^' || ch=='&'){
-    printf("Special Character");
+   else if(ch=='b' || ch=='c' || ch=='d' || ch=='f' || ch=='g' || ch=='h' || ch=='j' || ch=='k' || ch=='l' || ch=='m' || ch=='n' || ch=='p' || ch=='q' || ch=='r' || ch=='s' || ch=='t' ||ch=='v' || ch=='w' || ch=='x'|| ch=='y' || ch=='z'){
+    printf("Consonant");
    }
    else{
-    printf("Consonant");
+    printf("Special Character");
    }
 }
