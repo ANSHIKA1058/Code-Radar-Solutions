615540b022a5e28364e533b3a025e9da4ed4634e
ANSHIKA <anshika1058.becse24@chitkara.edu.in>
2025-04-07 15:18:38 +0530

    Solution to Find the Longest Word in a String

---
+++ Find the Longest Word in a String@@676c039d80e1714600f6764f/code.c
@@ -0,0 +1,26 @@
+// Your code here...
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+int main(){
+    char str[1000],word[100],longest[100];
+    int i=0,j=0,max=0;
+    fgets(str,sizeof(str),stdin);
+    while(str[i]!='\0'){
+        if(str[i]!=' ' && str[i] !='\n'){
+            word[j++]=str[i];
+        }else{
+            word[j]='\0';
+            if(j>max){
+                max=j;
+                for(int k=0;k<=j;k++){
+                    longest[k]=word[k];
+                }
+            }
+            j=0;
+        }
+        i++;
+    }
+    printf("%s",longest);
+}
+}
